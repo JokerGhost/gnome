9d9fc23144d876e9800c9be823bf24d8c5df6584
JokerGhost <46416794+JokerGhost@users.noreply.github.com>
2020-05-06 18:42:38 +0630

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+apt-get install gnome-desktop-environment
+
+
+apt-get install xfonts-100dpi
+
+apt-get install xfonts-100dpi-transcoded
+
+apt-get install xfonts-75dpi
+
+apt-get install xfonts-75dpi-transcoded
+
+apt-get install xfonts-base
+
+
+apt-get install tightvncserver
+
+tightvncserver :1
+
+tightvncserver -kill :1
+done
